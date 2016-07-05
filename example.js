52124e95ace859e223517613cd30b09734ae152b
Jonathan Lake <jwtlake@gmail.com>
2016-07-05 02:08:25 -0700

    saving work

--- example.js
+++ example.js
@@ -1,16 +1,19 @@
-import {Library, Book, Librarian} from './index.js'
+// import { Library, Book } from './lib/simple-book-library.js';
+var simple = require('./lib/simple-book-library.js');
+
 var util = require('util');
 
 //create new library (numbOfShelves, numbOfSlots)
-var MyLibary = new Library(10,5);
+var MyLibary = new simple.Library(10,5);
 
 //create a books (name, haveRead)
-var book1 = new Book('How to javascript',true);
-var book2 = new Book('How to javascript while being watched',false);
+var book1 = new simple.Book('How to javascript',true);
+var book2 = new simple.Book('How to javascript while being watched',false);
 
 //put the book in the libary (Book, shelfNumb, rowNumber)
-MyLibary.shelf(book1,1,5);
+MyLibary.shelf(book1,1,4);
 MyLibary.shelf(book2,2,1);
 
 //output full array
+MyLibary.render();
 console.log(util.inspect(MyLibary, {showHidden: false, depth: null}));
--- src/index.js
+++ src/index.js
@@ -1,44 +1,113 @@
 /* Library Object */ 
 export const Library = class Library {
-	constructor(shelfNumb, slotNumb) {
+
+	constructor(shelfNumb = 8, slotNumb = 10) {
 		this.shelfs = [];
-		this._makeShelfs(shelfNumb);
+		this.maxShelf = (this.shelfNumb - 1);
+		this.maxSlot = (this.slotNumb - 1);
+		this._createShelfs(shelfNumb, slotNumb);
 	}
-	_makeShelfs(number){
-		for(var i = 0; i < number; i++){
-			var newShelf = new Shelf();
+	
+	_createShelfs(shelfNumb, slotNumb) {
+		// create shelfs
+		for(var i = 0; i < shelfNumb; i++) {
+			var newShelf = new Shelf(slotNumb);
 			this.shelfs.push(newShelf);
 		}
 	}
-	shelf(book,shelfNumb,slotNumb){
-	 var selectedShelf = this.shelfs[shelfNumb];
-	 return selectedShelf._putBook(book,slotNumb);
+
+	_isValidSelection(shelfNumb,slotNumb) {
+		
+		// check for valid shelf selection
+		if(shelfNumb <= 0 || shelfNumb > this.maxShelf){
+			console.log(`Invalid shelf selection! Please choose a shelf number between 0 and ${this.maxShelf}`);
+			return false;
+		}
+		
+		// check for valid slot selection
+		if(slotNumb <= 0 || slotNumb > this.maxSlot){
+			console.log(`Invalid slot selection! Please choose a slot number between 0 and ${this.maxSlot}`);
+			return false;
+		}
+		// return valid
+		return true;
 	}
-	unshelf(shelfNumb,slotNumb){
-		return this.shelfs[shelf]._removeBook(slotNumb);
+	
+	shelf(book,shelfNumb,slotNumb) {
+		if(this._isValidSelection) {
+	 		return this.shelfs[shelfNumb]._putBook(book,slotNumb);
+		}else{
+			return false;
+		}
+	}
+	
+	unshelf(shelfNumb,slotNumb) {
+		if(this._isValidSelection) {
+	 		return this.shelfs[shelfNumb]._removeBook(slotNumb);
+		}else{
+			return false;
+		}
 	}
 
+	getShelf() {
+		return this.shelfs;
+	}
+
+	render() {
+		// header
+		console.log('## Library');
+		console.log();
+		const slotHeaders = Array.apply(null, {length: this.maxSlot}).map(Number.call, Number).join(' ');
+		console.log(`SLOTS:    ${slotHeaders}`);
+		
+		// body
+		for (let [index, selectedShelf] of this.shelfs.entries()) {
+			const shelfContents = selectedShelf.getSlots().map(book => {
+				if(book){return 'B';}
+				else{return '_';}
+			}).join('  ');
+			console.log(`SHELF ${index}: ${shelfContents}`);
+		}
+	}
 }
 
 /* Shelf Object */ 
 export const Shelf = class Shelf {
 	
-	constructor() {
+	constructor(slotNumb) {
 		this.slots = [];
+		this._createSlots(slotNumb);
 	}
-  		
-	_putBook(book,slotNumb){
-		//check if slot full
-		var bookCheck = this.slots[slotNumb];
-		if(bookCheck){
-			return book; //give back book.. maybe dont need this.
-		}else{
-			this.slots[slotNumb] = book;
-			return //success
+
+	_createSlots(slotNumb) {
+		for(let i = 0; i < slotNumb; i++){
+			this.slots.push(null);
 		}
 	}
-	_removeBook(slotNumb){
-		return this.slots[slotNumb];
+  		
+	_putBook(book,slotNumb) {
+		//check if slot full
+		const isBook = this.slots[slotNumb];
+		if(!isBook){
+			return false;
+		}else{
+			this.slots[slotNumb] = book;
+			return true;
+		}
+	}
+	_removeBook(slotNumb) {
+		const bookToRemove = this.slots[slotNumb];
+		if(!bookToRemove){
+			console.log('There is no book at this location.');
+			return false;
+		}else{
+			this.slots[slotNumb] = null;
+			return bookToRemove
+		}
+	}
+
+	getSlots() {
+		return this.slots;
 	}
 }
 
@@ -50,22 +119,8 @@ export const Book = class Book {
   }
 }
 
-/* Librarian Object */
-export const Librarian = class Librarian {
-  constructor() {
-  }
-  sort(){
-    //categorize
-    //alphabetize
-  }
-  lookup(bookName){
-
-  }
-}
-
 export default {
   Library: Library,
   Shelf: Shelf,
-  Book: Book,
-  Librarian: Librarian
+  Book: Book
 }
