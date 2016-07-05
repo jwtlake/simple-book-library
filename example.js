0435bab71d75db136b0a6ec63d4dd1aec531a237
Jonathan Lake <jwtlake@gmail.com>
2016-07-05 13:46:17 -0700

    example is in es5 so switching to var

--- example.js
+++ example.js
@@ -1,10 +1,10 @@
 // es6 imports also supported 
 //import { Library, Book } from './lib/simple-book-library.js';
 
-let simple = require('./lib/simple-book-library.js');
+var simple = require('./lib/simple-book-library.js');
 
 // create new library (numbOfShelves, numbOfSlots)
-let MyLibary = new simple.Library(10,5);
+var MyLibary = new simple.Library(10,5);
 
 // if you want to dissable passive console.logs on libary actions
 //MyLibary.logPassiveOutput = false; 
@@ -13,9 +13,9 @@ let MyLibary = new simple.Library(10,5);
 MyLibary.render();
 
 // create a books (name)
-let book1 = new simple.Book('A Game of Thrones');
-let book2 = new simple.Book('A Clash of Kings');
-let book3 = new simple.Book('A Storm of Swords');
+var book1 = new simple.Book('A Game of Thrones');
+var book2 = new simple.Book('A Clash of Kings');
+var book3 = new simple.Book('A Storm of Swords');
 
 //put the books in the libary (Book, shelfNumb, rowNumber)
 MyLibary.shelf(book1,11,1) // out of bounds
@@ -25,7 +25,7 @@ MyLibary.shelf(book2,2,1); // valid
 MyLibary.shelf(book3,1,4); // occupied
 
 //move book
-let bookInHand = MyLibary.unshelf(1,4);
+var bookInHand = MyLibary.unshelf(1,4);
 MyLibary.shelf(bookInHand,2,4);
 
 //try again
