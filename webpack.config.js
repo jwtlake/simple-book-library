b1e33083af231dc7e3a12495eb7177cfd5a8ca02
Jonathan Lake <jwtlake@gmail.com>
2016-07-04 23:36:23 -0700

    updated build processe

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,18 @@
 var webpack = require('webpack');
+var UglifyJsPlugin = webpack.optimize.UglifyJsPlugin;
 var path = require('path');
+var env = require('yargs').argv.mode;
+
 var libraryName = 'simple-book-library';
-var outputFile = libraryName + '.js';
+
+var plugins = [], outputFile;
+
+if (env === 'build') {
+  plugins.push(new UglifyJsPlugin({ minimize: true }));
+  outputFile = libraryName + '.min.js';
+} else {
+  outputFile = libraryName + '.js';
+}
 
 var config = {
   entry: __dirname + '/src/index.js',
@@ -25,7 +36,8 @@ var config = {
   resolve: {
     root: path.resolve('./src'),
     extensions: ['', '.js']
-  }
+  },
+  plugins: plugins
 };
 
 module.exports = config;
