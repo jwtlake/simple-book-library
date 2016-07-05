8a7e60495e3a4d6f0d88402ea78d8fcc7e38505f
Jonathan Lake <jwtlake@gmail.com>
2016-07-05 14:16:13 -0700

    fixed spelling errors and added shelf/unshelf info

--- example.js
+++ example.js
@@ -4,32 +4,42 @@
 var simple = require('./lib/simple-book-library.js');
 
 // create new library (numbOfShelves, numbOfSlots)
-var MyLibary = new simple.Library(10,5);
+var MyLibrary = new simple.Library(10,5);
 
-// if you want to dissable passive console.logs on libary actions
-//MyLibary.logPassiveOutput = false; 
+// if you want to disable passive console.logs on library actions
+//MyLibrary.logPassiveOutput = false; 
 
-//output libary
-MyLibary.render();
+//output library
+MyLibrary.render();
 
-// create a books (name)
+// create some books (name)
 var book1 = new simple.Book('A Game of Thrones');
 var book2 = new simple.Book('A Clash of Kings');
 var book3 = new simple.Book('A Storm of Swords');
 
-//put the books in the libary (Book, shelfNumb, rowNumber)
-MyLibary.shelf(book1,11,1) // out of bounds
-MyLibary.shelf(book1,1,5) // out of bounds
-MyLibary.shelf(book1,1,4); // valid
-MyLibary.shelf(book2,2,1); // valid
-MyLibary.shelf(book3,1,4); // occupied
+//put the books in the library (Book, shelfNumb, rowNumber)
+MyLibrary.shelf(book1,11,1) // out of bounds
+MyLibrary.shelf(book1,1,5) // out of bounds
+MyLibrary.shelf(book1,1,4); // valid
+MyLibrary.shelf(book2,2,1); // valid
+MyLibrary.shelf(book3,1,4); // occupied
 
 //move book
-var bookInHand = MyLibary.unshelf(1,4);
-MyLibary.shelf(bookInHand,2,4);
+var bookInHand = MyLibrary.unshelf(1,4);
+MyLibrary.shelf(bookInHand,2,4);
 
 //try again
-MyLibary.shelf(book3,1,4); // valid
+MyLibrary.shelf(book3,1,4); // valid
 
-//output libary
-MyLibary.render();
+//output library
+MyLibrary.render();
+
+//Library.shelf() => returns:
+//true -- If successful
+//false -- If out of bounds request
+//Book obj -- If book is present at location (existing 'blocking' book remains in place but is returned for reference)
+
+//Library.unshelf() => returns:
+//Book obj -- If successful (requested book is returned and the empty slot is replaced with a null value)
+//false -- If out of bounds request
+//false -- If no book is present)
